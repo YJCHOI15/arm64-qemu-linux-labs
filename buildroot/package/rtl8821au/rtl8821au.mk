f2a0668fa54c63d8f95a4ca69f5171f46bc42343
YJCHOI15 <ccyjin15@gmail.com>
2025-08-08 02:33:29 +0900

    add buildroot again

---
+++ buildroot/package/rtl8821au/rtl8821au.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# rtl8821au
+#
+################################################################################
+
+RTL8821AU_VERSION = d0a49ba401a8661ef0cdc4cf8850bc7c927a062c
+RTL8821AU_SITE = $(call github,benetti-engineering-sas,rtl8821au,$(RTL8821AU_VERSION))
+RTL8821AU_LICENSE = GPL-2.0
+RTL8821AU_LICENSE_FILES = LICENSE
+
+define RTL8821AU_LINUX_CONFIG_FIXUPS
+	$(call KCONFIG_ENABLE_OPT,CONFIG_NET)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_WIRELESS)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_CFG80211)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_USB_SUPPORT)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_USB)
+endef
+
+RTL8821AU_MODULE_MAKE_OPTS = \
+	CONFIG_RTL8812AU_8821AU=m \
+	KVER=$(LINUX_VERSION_PROBED) \
+	USER_EXTRA_CFLAGS="-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN \
+		-Wno-error"
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
